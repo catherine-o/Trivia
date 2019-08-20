76784b38ae303f2c4048ff7dd439374ade499d02
Jerkbert <raballantyne@gmail.com>
2019-08-20 16:54:09 -0600

    render new question works, getting to correct answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,17 @@ class Question < ApplicationRecord
     has_many :answers
     has_many :teams_answers
     has_many :teams, through: :teams_answers
-
+    
+    
     
 
 end
+
+
+# <% if @answer.text == @question.answer %>
+#     <h1> Correct!<h1>
+# <% elsif @answer.text != @question.answer %>
+#     <h1> WRONG!<h1>
+# <% else %>
+# <% end %>
+#     
