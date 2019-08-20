6fd9bb9a06f7a207f3765e008ac32e1d60107beb
catherine-o <catherine.ohara@outlook.com>
2019-08-20 10:50:36 -0600

    added seeds and some view pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,89 @@ require 'cgi'
 require 'json'
 
 Question.destroy_all
+Answer.destroy_all
+Team.destroy_all
+Round.destroy_all
+RoundsQuestion.destroy_all
 
-base_url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple"
+easy_base_url = "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple"
 
-results = RestClient.get(base_url)
-results_array = JSON.parse(results)["results"]
+easy_results = RestClient.get(easy_base_url)
+easy_results_array = JSON.parse(easy_results)["results"]
 
-results_array.each do |result|
+easy_results_array.each do |result|
     Question.create(
         question_text: CGI.unescapeHTML(result["question"]),
         difficulty: result["difficulty"],
         category: result["category"],
-        point_value: 1
+        point_value: 5,
+        answer: result["correct_answer"]
     )
-    
 end
+
+medium_base_url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple"
+
+medium_results = RestClient.get(medium_base_url)
+medium_results_array = JSON.parse(medium_results)["results"]
+
+medium_results_array.each do |result|
+    Question.create(
+        question_text: CGI.unescapeHTML(result["question"]),
+        difficulty: result["difficulty"],
+        category: result["category"],
+        point_value: 7,
+        answer: result["correct_answer"]
+    )
+end
+
+hard_base_url = "https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple"
+
+hard_results = RestClient.get(hard_base_url)
+hard_results_array = JSON.parse(hard_results)["results"]
+
+hard_results_array.each do |result|
+    Question.create(
+        question_text: CGI.unescapeHTML(result["question"]),
+        difficulty: result["difficulty"],
+        category: result["category"],
+        point_value: 12,
+        answer: result["correct_answer"]
+    )
+end
+
+
+Team.create(team_name: "Rockets")
+Team.create(team_name: "Nuggets")
+Team.create(team_name: "Programmers")
+
+round1 = Round.create(name: "Round 1")
+round2 = Round.create(name: "Round 2")
+round3 = Round.create(name: "Round 3")
+
+rq1 = RoundsQuestion.create(question_id: 1, round: round1)
+rq2 = RoundsQuestion.create(question_id: 2, round: round1)
+RoundsQuestion.create(question_id: 3, round: round1)
+RoundsQuestion.create(question_id: 4, round: round1)
+RoundsQuestion.create(question_id: 5, round: round1)
+RoundsQuestion.create(question_id: 6, round: round1)
+RoundsQuestion.create(question_id: 7, round: round1)
+RoundsQuestion.create(question_id: 8, round: round1)
+RoundsQuestion.create(question_id: 9, round: round1)
+RoundsQuestion.create(question_id: 10, round: round1)
+
+RoundsQuestion.create(question_id: 11, round: round2)
+RoundsQuestion.create(question_id: 12, round: round2)
+RoundsQuestion.create(question_id: 13, round: round2)
+RoundsQuestion.create(question_id: 14, round: round2)
+RoundsQuestion.create(question_id: 15, round: round2)
+RoundsQuestion.create(question_id: 16, round: round2)
+RoundsQuestion.create(question_id: 17, round: round2)
+RoundsQuestion.create(question_id: 18, round: round2)
+RoundsQuestion.create(question_id: 19, round: round2)
+RoundsQuestion.create(question_id: 20, round: round2)
+
+
+
 byebug
     0
 
