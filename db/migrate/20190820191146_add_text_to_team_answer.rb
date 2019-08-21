ac8c8f69a8dee797ec88bc1d489163fbb321d782
catherine-o <catherine.ohara@outlook.com>
2019-08-21 17:04:15 -0600

    deleted teamanswer migrations

--- db/migrate/20190820191146_add_text_to_team_answer.rb
+++
@@ -1,5 +0,0 @@
-class AddTextToTeamAnswer < ActiveRecord::Migration[5.2]
-  def change
-    add_column :team_answers, :text, :string
-  end
-end
--- db/migrate/20190821202822_drop_team_answers.rb
+++
@@ -1,5 +0,0 @@
-class DropTeamAnswers < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :team_answers
-  end
-end
